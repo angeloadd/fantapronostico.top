9037c515be285a3651982473a9852a421977354f
Angelo <angelo.adduci1991@gmail.com>
2024-06-03 22:56:26 +0200

    chores: add logging on email sending

--- app/Modules/Auth/Http/Controllers/SendVerificationLinkEmailAction.php
+++ app/Modules/Auth/Http/Controllers/SendVerificationLinkEmailAction.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Modules\Auth\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 
 final class SendVerificationLinkEmailAction
 {
@@ -15,6 +17,15 @@ final class SendVerificationLinkEmailAction
             return new Response(null, 302, ['HX-Location' => route('home')]);
         }
 
+        try {
+            $request->user()->sendEmailVerificationNotification();
+        } catch (\Throwable $e) {
+            Log::error($e->getMessage(), [
+                'exception' => $e,
+            ]);
+
+            return new Response(view('components.partials.notifications.toast', ['type' => 'success', 'text' => 'Qualcosa è andato storto'])->render());
+        }
         $request->user()->sendEmailVerificationNotification();
 
         return new Response(view('components.partials.notifications.toast', ['type' => 'success', 'text' => 'Email mandata con successo'])->render());
