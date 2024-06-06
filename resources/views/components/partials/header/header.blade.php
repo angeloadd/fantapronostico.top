52ccf24693f6a8442d93e2f17b307ec4bb4bdd32
Angelo <angelo.adduci1991@gmail.com>
2024-06-06 19:57:55 +0200

    chores: fix ranking view

--- resources/views/components/partials/header/header.blade.php
+++ resources/views/components/partials/header/header.blade.php
@@ -1,4 +1,4 @@
-<div class="{{ $bgColor }} sm:px-20 min-h-32 max-h-48 w-full flex justify-start items-center p-6 shadow-lg text-base-100 bg-gradient-to-b from-neutral/30 via-transparent">
+<div class="{{ $bgColor }} sm:px-20 min-h-32 max-h-48 w-full flex justify-center items-start p-6 shadow-lg text-base-100 bg-gradient-to-b from-neutral/30 via-transparent">
     <h1 class="text-3xl sm:text-4xl font-bold uppercase">{{$text}}</h1>
     <img class="hidden sm:block h-full" src="{{Vite::asset('resources/assets/images/'.$img)}}" alt="{{$alt}}">
 </div>
