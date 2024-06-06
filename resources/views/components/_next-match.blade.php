48c76bedf8fcf653093785527e81366e32383a06
Angelo <angelo.adduci1991@gmail.com>
2024-06-06 19:55:43 +0200

    chores: fix images for next match card

--- resources/views/components/_next-match.blade.php
+++ resources/views/components/_next-match.blade.php
@@ -4,15 +4,15 @@
     </div>
     <div class="flex flex-col justify-center items-center">
         <div class="flex justify-between items-center w-full">
-            <div>
-                <div class="w-12 sm:w-36 mask mask-circle">
+            <div class="w-full flex flex-col justify-center items-center">
+                <div class="w-12 h-12 sm:h-36 sm:w-36 flex justify-center items-center">
                     <img
                         src="{{$nextGame->home_team->logo}}"
-                        class="object-center object-fill"
-                        alt="{{__(__($nextGame->away_team->name))}}-Flag"
+                        class="object-cover object-center overflow-hidden"
+                        alt="{{$nextGame->home_team->name}} Flag"
                     >
                 </div>
-                <h3 class="text-xl font-bold text-center">{{__($nextGame->home_team->name)}}</h3>
+                <h3 class="text-lg font-bold text-center whitespace-nowrap">{{__($nextGame->home_team->name)}}</h3>
             </div>
             <div class="text-neutral/50 text-center">
                 <p class="font-bold">
@@ -22,15 +22,15 @@
                     {{$nextGame->started_at->format('H:i')}}
                 </p>
             </div>
-            <div>
-                <div class="w-12 sm:w-36 mask-circle mask">
+            <div class="flex flex-col w-full justify-center items-center">
+                <div class="w-12 h-12 sm:h-36 sm:w-36 flex justify-center items-center">
                     <img
                         src="{{$nextGame->away_team->logo}}"
-                        class="object-fill object-center"
+                        class="object-cover object-center overflow-hidden"
                         alt="{{$nextGame->away_team->name}} Flag"
                     >
                 </div>
-                <h3 class="text-xl font-bold text-center">{{__($nextGame->away_team->name)}}</h3>
+                <h3 class="text-lg font-bold text-center whitespace-nowrap">{{__($nextGame->away_team->name)}}</h3>
             </div>
         </div>
 
