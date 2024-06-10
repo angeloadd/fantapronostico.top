cfc97008e4aaa90d6f65fe3f55f8effae1257baf
Angelo <angelo.adduci1991@gmail.com>
2024-06-10 14:01:51 +0200

    chores: check schedule

--- app/Modules/ApiSport/Console/console.php
+++ app/Modules/ApiSport/Console/console.php
@@ -11,38 +11,30 @@ try {
     /** @var ?Carbon $finalStartedAt */
     $finalStartedAt = Tournament::first()?->final_started_at;
     if ($finalStartedAt->addHours(10)->isFuture()) {
-        Schedule::call(static fn() => Log::info('schedule is running'));
+        Schedule::call(static fn() => Log::info('schedule is running'))->everyMinute();
         Schedule::command('fp:teams:get')
-            ->timezone('Europe/Rome')
             ->dailyAt('01:05');
         Schedule::command('fp:fetch:games')
-            ->timezone('Europe/Rome')
             ->dailyAt('01:10');
         Schedule::command('fp:fetch:players')
-            ->timezone('Europe/Rome')
             ->dailyAt('01:15');
         Schedule::command('fp:players:fix')
-            ->timezone('Europe/Rome')
             ->dailyAt('02:15');
 
         Schedule::command('fp:fetch:games:events')
             ->hourlyAt(['15', '30'])
-            ->timezone('Europe/Rome')
             ->between('17:00', '18:00');
         Schedule::command('fp:fetch:games:events')
             ->hourlyAt(['15', '30'])
-            ->timezone('Europe/Rome')
             ->between('19:00', '10:00');
         Schedule::command('fp:fetch:games:events')
             ->hourlyAt(['15', '30'])
-            ->timezone('Europe/Rome')
             ->between('23:00', '23:50');
 
         $isPast = $finalStartedAt?->isPast();
         $areAllGamesFinished = Game::all()->every('status', '=', 'finished');
         if ($isPast && $areAllGamesFinished) {
             Schedule::command('fp:fetch:champions')
-                ->timezone('Europe/Rome')
                 ->everyThreeMinutes();
         }
     }
