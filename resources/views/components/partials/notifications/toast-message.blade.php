d2b2b068bc517ae317d2ef7ee496b43a5247833d
Angelo <angelo.adduci1991@gmail.com>
2024-06-15 23:24:59 +0200

    chores: fix alpine error for teletransport

--- resources/views/components/partials/notifications/toast-message.blade.php
+++ resources/views/components/partials/notifications/toast-message.blade.php
@@ -1,11 +1,17 @@
-<template x-teleport="#toastWrapper">
-    @if(null !== session($sessionKey ?? 'message'))
+@if(null !== session($sessionKey ?? 'message'))
+    <template x-teleport="#toastWrapper">
         <x-partials.notifications.toast :text="session($sessionKey ?? 'message')" type="success"/>
-    @elseif(null !== session($errorKey ?? 'error_message'))
+    </template>
+@elseif(null !== session($errorKey ?? 'error_message'))
+    <template x-teleport="#toastWrapper">
         <x-partials.notifications.toast :text="session($errorKey ?? 'error_message')" type="error"/>
-    @elseif(null !== session($warningKey ?? 'warning_message'))
+    </template>
+@elseif(null !== session($warningKey ?? 'warning_message'))
+    <template x-teleport="#toastWrapper">
         <x-partials.notifications.toast :text="session($warningKey ?? 'warning_message')" type="warning"/>
-    @elseif(null !== session($infoKey ?? 'info_message'))
+    </template>
+@elseif(null !== session($infoKey ?? 'info_message'))
+    <template x-teleport="#toastWrapper">
         <x-partials.notifications.toast :text="session($infoKey ?? 'info_message')" type="info"/>
-    @endif
-</template>
+    </template>
+@endif
