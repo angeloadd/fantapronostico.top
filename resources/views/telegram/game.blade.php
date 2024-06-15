4ac5c026ff0cebd50fb6e884cd2c2501df08286b
Angelo <angelo.adduci1991@gmail.com>
2024-06-15 14:39:30 +0200

    chores: add translations for telegram bot

--- resources/views/telegram/game.blade.php
+++ resources/views/telegram/game.blade.php
@@ -1,5 +1,5 @@
 <a href="{{route('prediction.create', compact('game'))}}">
     {{route('prediction.create', compact('game'))}}
 </a>
-<strong>{{$game->home_team->name}}</strong> - <strong>{{$game->away_team->name}}</strong>
+<strong>{{__($game->home_team->name)}}</strong> - <strong>{{__($game->away_team->name)}}</strong>
 <strong>{{str($game->started_at->isoFormat('\e\n\t\r\o \i\l D MMMM YYYY \a\l\l\e HH:mm'))->title()}}</strong>
