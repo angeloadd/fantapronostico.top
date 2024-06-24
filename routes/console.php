6518b4aaa9b63736e71d5e651953b7033b65cd42
Angelo <angelo.adduci1991@gmail.com>
2024-06-24 09:51:30 +0200

    chores: remove inspire to have less schedules

--- routes/console.php
+++ routes/console.php
@@ -4,14 +4,6 @@ declare(strict_types=1);
 
 use App\Models\Game;
 use App\Models\Tournament;
-use Illuminate\Foundation\Console\ClosureCommand;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
-
-Artisan::command('inspire', function (): void {
-    /** @var ClosureCommand $this */
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
 
 $doScheduleBeforeTournamentIsFinished = static fn () => Tournament::first()
     ?->final_started_at
