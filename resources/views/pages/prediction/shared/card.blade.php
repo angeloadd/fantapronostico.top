6702af3857fcb66b702ab2be97f374b7d5b6b203
Angelo <angelo.adduci1991@gmail.com>
2024-06-28 19:08:24 +0200

    chores: fix card for prediction show

--- resources/views/pages/prediction/shared/card.blade.php
+++ resources/views/pages/prediction/shared/card.blade.php
@@ -1,4 +1,4 @@
-<div class="card w-full max-w-xl shadow-2xl rounded-xl bg-base-100 border-base-300">
+<div class="card w-full @if($large ?? null) max-w-2xl @else max-w-xl @endif shadow-2xl rounded-xl bg-base-100 border-base-300">
     <div class="card-body p-2 sm:p-8">
         {{$slot}}
     </div>
--- resources/views/pages/prediction/shared/table.blade.php
+++ resources/views/pages/prediction/shared/table.blade.php
@@ -1,5 +1,5 @@
 <div class="w-full overflow-auto sm:overflow-visible">
-    <table class="table table-zebra text-md">
+    <table class="table table-zebra">
         <x-prediction::shared.table-head :$game/>
         <tbody>
             {{$slot}}
